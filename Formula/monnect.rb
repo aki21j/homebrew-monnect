6bd0636793eddec97861d1a626e348b75dea3cad
Ankit Gupta <ankitgupta21j@gmail.com>
2026-02-20 23:05:46 +0000

    Add monnect 1.0.0 formula

---
+++ Formula/monnect.rb
@@ -0,0 +1,17 @@
+class Monnect < Formula
+  desc "Automatically connect or disconnect a Bluetooth speaker when a specific display is connected on macOS"
+  homepage "https://github.com/aki21j/Monnect"
+  url "https://files.pythonhosted.org/packages/source/m/monnect/monnect-1.0.0.tar.gz"
+  sha256 "eadddeedf82e7d59f7ac91000bfe69dc296ae54780fcad3dde868fd498710a45"
+  license "MIT"
+
+  depends_on "python@3.12"
+
+  def install
+    system Formula["python@3.12"].opt_bin/"pip3", "install", *std_pip_args
+  end
+
+  test do
+    system "#{bin}/monnect", "--version"
+  end
+end
